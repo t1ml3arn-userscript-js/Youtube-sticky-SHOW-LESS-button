d2a9cbb623d1323a8a7e40b4b0cbda00713f870b
T1mL3arn <T1mL3arn@users.noreply.github.com>
2022-06-10 00:06:56 +1000

    release v1.1.1

--- youtube-sticky-show-less.user.js
+++ youtube-sticky-show-less.user.js
@@ -22,7 +22,7 @@ Copyright (C) 2022  T1mL3arn
 // @description Makes SHOW LESS button to be "sticky" to the video description section, so you can easily fold a long description without scrolling it all the way to its bottom.
 // @description:RU Делает кнопку СВЕРНУТЬ в описании видео "липкой". Чтобы свернуть длинное описание теперь не нужно прокручивать это описание в самый низ.
 // @namespace   https://github.com/t1ml3arn-userscript-js
-// @version     1.1.0
+// @version     1.1.1
 // @match				https://www.youtube.com/*
 // @match       https://youtube.com/*
 // @grant       none
